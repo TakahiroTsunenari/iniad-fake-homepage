9c3621eae6c3d73ca822fd799f1d202fcd1d9422
HIRUMAINIAD <toda2023-019@iniad.org>
2024-01-24 21:34:06 +0900

    複数ページで自動切替できるように変更
    page_switch.jsの内容を変更し、現在のページを取得し、次のページに画面遷移するように改良。
    
    map.htmlにもpage_switch.jsを読み込むようにscriptタグを追加。

--- signage/js/page_switch.js
+++ signage/js/page_switch.js
@@ -1,3 +1,23 @@
-﻿setTimeout(function(){
-    window.location.href = 'map.html';
-  }, 10*1000);
+﻿// 自動切替したいページのリスト
+// ページを追加、ファイル名を変更した場合はここを変更する
+const signagePages = ['index.html','map.html'];
+
+// 次のページに切り替わるまでの秒数
+const timeoutSeconds = 5;
+
+const currentPage = location.pathname.split('/')[2];
+const nextPageIndex = signagePages.indexOf(currentPage)+1;
+const numberOfPages = signagePages.length;
+let nextPage = '';
+
+// 次のページが要素数を超えてしまったら、最初のページへ
+if (nextPageIndex >= numberOfPages){
+    nextPage = signagePages[nextPageIndex - numberOfPages];
+}else{
+    nextPage = signagePages[nextPageIndex];
+}
+
+// 指定秒後に次のページへ
+setTimeout(function(){
+    window.location.href = nextPage;
+  }, timeoutSeconds*1000);
